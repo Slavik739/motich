f9740bfded1d04e07a9018702c50199b2e44777a
Slavik <svstason@gmail.com>
2022-01-11 00:26:15 +0200

    find square house

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-alert('hi')
+const lenghtFirstRoom = prompt('Введите длину вашей 1й комнаты');
+const weightFirstRoom = prompt('Введите ширину вашей 1й комнаты');
+const lenghtSecondRoom = prompt('Введите длину вашей 2й комнаты');
+const weightSecondRoom = prompt('Введите ширину вашей 2й комнаты');
+
+const squareHouse = (lenghtFirstRoom * weightFirstRoom) + (lenghtSecondRoom * weightSecondRoom)
+
+alert("Площадь вашего дома - " + squareHouse + "кв.м.")
