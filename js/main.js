7833cff394b494723a355ae7819fa3143d6f43c7
Slavik <svstason@gmail.com>
2022-01-30 21:38:01 +0200

    added in html - two button with function(calculate square and show code functionCreator)

--- js/main.js
+++ js/main.js
@@ -1,8 +1,50 @@
-const lenghtFirstRoom = prompt('Введите длину вашей 1й комнаты');
-const weightFirstRoom = prompt('Введите ширину вашей 1й комнаты');
-const lenghtSecondRoom = prompt('Введите длину вашей 2й комнаты');
-const weightSecondRoom = prompt('Введите ширину вашей 2й комнаты');
+let btnSquare = document.querySelector('.btnSquare');
+let btnCode = document.querySelector('.btnCode');
+let codePlace = document.querySelector('.code-place')
+let showCode = true
 
-const squareHouse = (lenghtFirstRoom * weightFirstRoom) + (lenghtSecondRoom * weightSecondRoom)
+btnSquare.addEventListener('click', function () {
+    const lenghtFirstRoom = prompt('Введите длину вашей 1й комнаты');
+    const weightFirstRoom = prompt('Введите ширину вашей 1й комнаты');
+    const lenghtSecondRoom = prompt('Введите длину вашей 2й комнаты');
+    const weightSecondRoom = prompt('Введите ширину вашей 2й комнаты');
 
-alert("Площадь вашего дома - " + squareHouse + "кв.м.")
+    const squareHouse = (lenghtFirstRoom * weightFirstRoom) + (lenghtSecondRoom * weightSecondRoom)
+
+    alert("Площадь вашего дома - " + squareHouse + "кв.м.")
+});
+
+btnCode.addEventListener('click', function () {
+    if (!showCode) {
+        codePlace.classList.add('past')
+        showCode = !showCode
+        }
+     else {
+    codePlace.classList.remove('past')
+        showCode = !showCode
+    }
+})
+
+insertCode();
+function insertCode(){
+    let code = document.createElement('pre');
+    code.innerHTML = `
+function SeasonsCreator(season = 'не задали'){
+    this.numberMounts=3;
+    this.nowYear=2022;
+    this.season=season;
+    this.welcomeSeason = function (){
+        console.log('\${this.numberMounts} месяца \${this.season} \${this.nowYear} - прекрасное время года!')
+    }
+}`
+codePlace.appendChild(code)
+}
+
+function SeasonsCreator(season = 'не задали'){
+    this.numberMounts=3;
+    this.nowYear=2022;
+    this.season=season;
+    this.welcomeSeason = function (){
+        console.log(`${this.numberMounts} месяца ${this.season} ${this.nowYear} - прекрасное время года!`)
+    }
+}
