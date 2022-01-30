c9da3069bf2d1efdd15a5f42b92c57bbf22f323f
Slavik <svstason@gmail.com>
2022-01-30 23:17:24 +0200

    added in JS class User - with get, set, howOld, fullName.

--- js/main.js
+++ js/main.js
@@ -18,15 +18,15 @@ btnCode.addEventListener('click', function () {
     if (!showCode) {
         codePlace.classList.add('past')
         showCode = !showCode
-        }
-     else {
-    codePlace.classList.remove('past')
+    } else {
+        codePlace.classList.remove('past')
         showCode = !showCode
     }
 })
 
 insertCode();
-function insertCode(){
+
+function insertCode() {
     let code = document.createElement('pre');
     code.innerHTML = `
 function SeasonsCreator(season = 'не задали'){
@@ -37,14 +37,47 @@ function SeasonsCreator(season = 'не задали'){
         console.log('\${this.numberMounts} месяца \${this.season} \${this.nowYear} - прекрасное время года!')
     }
 }`
-codePlace.appendChild(code)
+    codePlace.appendChild(code)
 }
 
-function SeasonsCreator(season = 'не задали'){
-    this.numberMounts=3;
-    this.nowYear=2022;
-    this.season=season;
-    this.welcomeSeason = function (){
+function SeasonsCreator(season = 'не задали') {
+    this.numberMounts = 3;
+    this.nowYear = 2022;
+    this.season = season;
+    this.welcomeSeason = function () {
         console.log(`${this.numberMounts} месяца ${this.season} ${this.nowYear} - прекрасное время года!`)
+    }
+}
+
+class User {
+    country = 'Ukraine';
+    colorSkin = 'yellow-blue';
+
+    constructor(name = '', surname = '', birthday = '', salary = '') {
+        this.name = name;
+        this.surname = surname;
+        this.birthday = birthday;
+        this._salary = +salary;
+    }
+
+    firstLastName() {
+        return `${this.name} ${this.surname}`
+    }
+
+    howYearsHumen() {
+        let date = this.birthday.split('.');
+        if (typeof date[2] !== "undefined") {
+            let dateRevert = date[2] + '.' + date[1] + '.' + date[0];
+            return ((new Date().getTime() - new Date(dateRevert)) / (24 * 3600 * 365.25 * 1000)) | 0;
+        }
+        return 0;
+    }
+
+    get salary() {
+        return this._salary
+    }
+
+    set salary(money) {
+        money > this._salary ? this._salary = money : ''
     }
 }
